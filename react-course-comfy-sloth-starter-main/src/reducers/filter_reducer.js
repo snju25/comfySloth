a092a91602b91f5cfa8b04a4380688f201becc0b
Sanjay Bhandari <bhandarisanjay879@gmail.com>
2023-12-09 22:30:37 +1000

    9/12/2023

--- react-course-comfy-sloth-starter-main/src/reducers/filter_reducer.js
+++ react-course-comfy-sloth-starter-main/src/reducers/filter_reducer.js
@@ -11,7 +11,58 @@ import {
 
 const filter_reducer = (state, action) => {
   if(action.type === LOAD_PRODUCTS){
-    return {...state,all_products : [...action.payload], filter_products: [...action.payload]}
+    let maxPrice = action.payload.map((p)=>p.price)
+    maxPrice = Math.max(...maxPrice)
+    return {...state,all_products : [...action.payload], filter_products: [...action.payload],
+    filters:{...state.filters,max_price : maxPrice, price: maxPrice}}
+  }
+  if(action.type === SET_GRIDVIEW){
+    return {...state, grid_view:  true}
+  }
+  if(action.type === SET_LISTVIEW){
+    return {...state, grid_view:  false}
+  }
+  if(action.type=== UPDATE_SORT){
+    return {...state, sort: action.payload}
+  }
+  if(action.type === SORT_PRODUCTS){
+    const {sort,filter_products} = state
+    let tempProducts = [...filter_products];
+    if(sort ==='price-lowest'){
+      tempProducts = tempProducts.sort((a,b)=>{
+        return a.price - b.price
+      })
+    }
+    if(sort ==='price-highest'){
+      tempProducts = tempProducts.sort((a,b)=>{
+        return b.price - a.price
+      })
+
+    }
+    if(sort ==='name-a'){
+      tempProducts = tempProducts.sort((a,b)=>{
+        return a.name.localeCompare(b.name)
+      })
+
+    }
+    if(sort ==='name-z'){
+      tempProducts = tempProducts.sort((a,b)=>{
+        return b.name.localeCompare(a.name)
+      })
+
+    }
+    return {...state,filter_products: tempProducts}
+  }
+  if(action.type === UPDATE_FILTERS){
+    const {name,value} = action.payload
+    return {
+      ...state,
+      filters: {...state.filters,[name]: value}
+    }
+  }
+  if(action.type === FILTER_PRODUCTS){
+    console.log("filtering the product")
+    return {...state}
   }
   throw new Error(`No Matching "${action.type}" - action type`)
 }
