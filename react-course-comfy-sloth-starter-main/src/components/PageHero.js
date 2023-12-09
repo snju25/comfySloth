19c2fb797f8f34cdedb5cedb87a4bd399687d802
Sanjay Bhandari <bhandarisanjay879@gmail.com>
2023-12-09 18:30:06 +1000

    done sorting

--- react-course-comfy-sloth-starter-main/src/components/PageHero.js
+++ react-course-comfy-sloth-starter-main/src/components/PageHero.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 const PageHero = ({title, product}) => {
+  console.log(product)
   return <Wrapper>
     <div className="section-center">
       <h3>
