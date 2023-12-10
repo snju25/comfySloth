746c3284206317178fa67933de4f073f967c2635
Sanjay Bhandari <bhandarisanjay879@gmail.com>
2023-12-10 21:05:24 +1000

    finished cart

--- react-course-comfy-sloth-starter-main/src/components/AddToCart.js
+++ react-course-comfy-sloth-starter-main/src/components/AddToCart.js
@@ -6,6 +6,7 @@ import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
 const AddToCart = ({product}) => {
+  const {addToCart} = useCartContext()
   const {id,stock,colors} = product
   const [mainColor, setMainColor] = useState(colors[0])
   const [amount, setAmount] = useState(1);
@@ -46,7 +47,7 @@ const AddToCart = ({product}) => {
     </div>
     <div className="btn-container">
       <AmountButtons amount={amount} increase={increase} decrease={decrease}/>
-      <Link to="/cart" className="btn">Add to Cart</Link>
+      <Link to="/cart" className="btn" onClick={()=> addToCart(id,mainColor,amount,product)}>Add to Cart</Link>
     </div>
   </Wrapper>
 }
