48a9bce1a05caa0ff0b44b49ae04f8eba1153ee3
Sanjay Bhandari <bhandarisanjay879@gmail.com>
2023-12-12 12:38:09 +1000

    done

--- react-course-comfy-sloth-starter-main/src/components/CartButtons.js
+++ react-course-comfy-sloth-starter-main/src/components/CartButtons.js
@@ -8,7 +8,7 @@ import { useUserContext } from '../context/user_context'
 
 const CartButtons = () => {
   const {closeSideBar} = useProductsContext()
-  const {total_items} = useCartContext()
+  const {total_items, clearCart} = useCartContext()
   const {loginWithRedirect, myUser, logout} = useUserContext()
   return <Wrapper className='cart-btn-wrapper'>
     <Link to='/cart' className='cart-btn' onClick={closeSideBar}>
@@ -18,7 +18,13 @@ const CartButtons = () => {
         <span className='cart-value'>{total_items}</span>
       </span>
     </Link>
-    {myUser ? <button type='button' className='auth-btn' onClick={()=> logout({returnTo:window.location.origin})}>Logout <FaUserMinus /></button>
+    {myUser ? <button type='button' className='auth-btn' 
+              onClick={()=> {
+                clearCart()
+                logout({returnTo:window.location.origin})}
+              }
+                >
+                  Logout <FaUserMinus /></button>
     : <button type='button' className='auth-btn' onClick={loginWithRedirect}>Login <FaUserPlus /></button>}
   </Wrapper>
 }
--- react-course-comfy-sloth-starter-main/src/index.js
+++ react-course-comfy-sloth-starter-main/src/index.js
@@ -16,8 +16,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 <Auth0Provider
-    domain="dev-744lnqdwzclgrwuu.us.auth0.com"
-    clientId="XdMjHp4Tj8NhssSgDKfKDpDC1vW61Dau"
+    domain= {process.env.REACT_APP_AUTH_DOMAIN}
+    clientId= {process.env.REACT_APP_AUTH_CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
